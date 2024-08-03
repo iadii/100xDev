cd65ade4cce9216b7185a1b1a5600f27ae1a731c
iadii <mishraaditya3456@gmail.com>
2024-08-03 17:29:50 +0530

    thrd push

---
+++ lec/week2/sumUptoTen.js
@@ -0,0 +1,35 @@
+
+const express = require('express')
+const app = express()
+// const port = 5173
+const port = 3000
+
+
+function calculateSum(count){
+
+    let sum = 0;
+    for(let i = 1; i <= count; i++){
+
+        sum += i;
+    }
+    return sum
+}
+
+
+function handleFirstRequest(req, res){
+    let calculatedSum = calculateSum(100);
+    let ans = `calculated sum is ${calculatedSum}`
+    
+    res.send(ans)
+
+
+}
+
+app.get('/', handleFirstRequest)
+
+const started = () => {
+    console.log(`Example app listening on port ${port}`)
+  }
+  
+app.listen(port, started)
+
