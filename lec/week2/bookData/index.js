db06615f8feb3cdbcfb18a2e0a15637501ca5566
iadii <mishraaditya3456@gmail.com>
2024-09-10 17:28:34 +0530

    post and delet

--- lec/week2/bookData/index.js
+++ lec/week2/bookData/index.js
@@ -42,6 +42,7 @@ let books = [
       
 ]
 
+
 app.get('/books/:id', (req, res) => {
     const book = books.find(b => b.id === parseInt(req.params.id));
     if (!book) return res.status(404).send('Book not found');
@@ -64,6 +65,17 @@ app.post('/books', (req, res) => {
       res.status(201).json(newBook);
 })
 
+app.delete('/books/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const bookIndex = books.findIndex(b => b.id === id);
+    
+    if (bookIndex === -1) {
+        return res.status(404).send('Book not found');
+    }
+    
+    const deletedBook = books.splice(bookIndex, 1)[0];
+    res.json(deletedBook);
+});
 
 app.listen(port = 3000, () => {
     console.log(`app is running at http://localhost:${port}`)
