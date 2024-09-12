a8dea4fe010e811ff574bb551381dc4fead56f79
iadii <mishraaditya3456@gmail.com>
2024-09-12 17:27:46 +0530

    update

---
+++ lec/week2/Day02/requestMethod/index.js
@@ -0,0 +1,35 @@
+const exp = require('express');
+const app = exp();
+
+
+const calcy = (num) => {
+    let sum = 0;
+    for(let i = 1; i <= num; i++){
+
+        sum += i;
+    }
+    return sum
+}
+
+app.post('/', (req, res) => {
+    let count = req.query.count;
+    let cal = calcy(count)
+    res.send(cal.toString());
+})
+// since there are some other ways to send data to backend except query param
+// i. header 
+// ii. body
+app.post('/body', (req, res) => {
+    let count = req.body;
+    let cal = calcy(count);
+    res.send(cal.toString())
+})
+app.post('/header', (req, res) => {
+    let count = req.headers.count;
+    let cal = calcy(count);
+    res.send(cal.toString())
+})
+
+app.listen(port = 4000, () => {
+    console.log(`App is running at http://localhost:${port}`)
+})
