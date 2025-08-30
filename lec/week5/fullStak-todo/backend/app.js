d6b815599df4b32a5e1d68fd5c5a2fe5895c1269
Aditya Mishra <mishraaditya3456@gmail.com>
2025-08-30 22:52:50 +0530

    added db schema

--- lec/week5/fullStak-todo/backend/app.js
+++ lec/week5/fullStak-todo/backend/app.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
-const {createTodo, updatTodo} = require('./types')
+const {createTodo, updatTodo} = require('./validation/types')
+const todo = require('./models/db')
 require('dotenv').config();
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 4001; 
 
 app.use(express.json());
 app.use(cors())
 
-
+ 
 // for posting todos
 app.post('/postTodo', async(req, res) => {
     const createPayload = req.body;
---
+++ lec/week5/fullStak-todo/backend/models/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { boolean } = require('zod');
+require('dotenv').config();
+
+mongoose.connect(process.env.mongo_URI)
+
+const todosSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+})
+
+const todo = mongoose.model('todos',todosSchema) 
+
+module.exports = {
+    todo: todo
+}
