b59372f77511b3cfc432e297d71a61456e94d987
Aditya Mishra <mishraaditya3456@gmail.com>
2025-08-30 10:19:09 +0530

    zod validation

--- lec/week5/fullStak-todo/backend/app.js
+++ lec/week5/fullStak-todo/backend/app.js
@@ -1,22 +1,39 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
+const z = require('zod')
 require('dotenv').config();
 const PORT = process.env.PORT || 4001;
 
 app.use(express.json());
+app.use(cors())
+// for posting todos
 
-
-res.get('/todos', (req, res) => {
-    res.send('hello')
+const validateData = z.object({
+    name: z.string().max(10),
+    description: z.string()
 })
+const valiidateCompletion = z.object({
+    id: z.string()
+})
+
 app.post('/postTodo', (req, res) => {
-    const {name, description, done} = req.body;
+    const {name, description} = req.body;
 
     res.status.json({
         name: name,
-        description: description,
-        done: done
+        description: description, 
     })
+})
+
+// for getting todos
+res.get('/todos', (req, res) => {
+    res.send('hello')
+})
+
+// for marking specific todos completed
+app.put('/completed', (req, res) => {
+
 })
 
 app.listen(PORT)
