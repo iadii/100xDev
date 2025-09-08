4324f77ccf3f0335bb416eba9e3a101af0a44ca9
Aditya Mishra <mishraaditya3456@gmail.com>
2025-09-09 01:04:41 +0530

    diya aur bati hum 🤣

--- lec/week6/react-component-return/src/App.jsx
+++ lec/week6/react-component-return/src/App.jsx
@@ -10,17 +10,20 @@ function App() {
      setTitle( Math.floor(Math.random() * 10 + 1 ))
   }
   return (
-    <>
+    <div>
       <button onClick={updateTitle}>Click me to update the title</button>
       <br></br>
       <br></br>
-      <button onClick={() => { setMessage(Math.floor(Math.random() * 100 + 1 )) }}>click me to update message</button>
-      <Message msg={`Message is ${message}`} />
+      {/* <button onClick={() => { setMessage(Math.floor(Math.random() * 100 + 1 )) }}>click me to update message</button>
+      <Message msg={`Message is ${message}`} /> */}
       <Header title={"Aditya"} />
-      <Header title={title} />
+      <Header title={"Adii"} />
+      <Header title={"Ammy"} />
+      <Header title={"iadii"} />
+      {/* <Header title={title} /> */}
       <Header title={` My name is ${title}`} />
-    </>
+    </div>
   );
 }
 export default App;
- 
+  
