15d7c874c9477fb78c225e1b52c78f60ba659446
Aditya Mishra <mishraaditya3456@gmail.com>
2025-09-19 02:40:33 +0530

    card wrapper

---
+++ lec/week6/CardComponents/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import { CardWrapper } from "./components/CardWrapper";
+import { TextComponent } from "./components/TextComponent";
+
+function App() {
+
+  return (
+  <>
+    <CardWrapper innerComponent={TextComponent}/>
+  </>
+  )
+}
+
+export default App;
---
+++ lec/week6/CardComponents/src/components/CardWrapper.jsx
@@ -0,0 +1,7 @@
+export function CardWrapper ({innerComponent}){
+    return(
+        <div style={{border: "2px solid black"}}>
+                {innerComponent}
+        </div>
+    )
+}
