105fa3c898f732ace08e8f2530397dba9c7ce75d
Aditya Mishra <mishraaditya3456@gmail.com>
2025-09-19 02:55:40 +0530

    card wrapper

--- lec/week6/CardComponents/src/App.jsx
+++ lec/week6/CardComponents/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import { CardWrapper } from "./components/CardWrapper";
 import { TextComponent } from "./components/TextComponent";
 import { ParagraphComponent } from "./components/ParagraphComponent";
+import { ChildrenWrapper } from "./components/ChildrenWrapper";
+
 
 function App() {
   return (
@@ -11,6 +13,13 @@ function App() {
          but wrapper component is same  */}
       <CardWrapper innerComponent={<TextComponent />} />
       <CardWrapper innerComponent={<ParagraphComponent />} />
+
+{/* in real world we will write under component */}
+{/* in this wrapper there is inbuilt property of react called children */}
+{/* it will automatically calls whatever inside of that wrapper */}
+      <ChildrenWrapper>
+        hii there
+      </ChildrenWrapper>
     </>
   );
 }
---
+++ lec/week6/CardComponents/src/components/ChildrenWrapper.jsx
@@ -0,0 +1,7 @@
+export function ChildrenWrapper({children}){
+    return(
+        <div style={{border: "2px solid red"}}>
+            {children}
+        </div>
+    )
+}
