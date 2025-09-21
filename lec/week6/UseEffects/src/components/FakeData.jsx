a2032d6539a51c78d952439a3f9d1db4d375b634
Aditya Mishra <mishraaditya3456@gmail.com>
2025-09-21 22:21:39 +0530

    poetry api fetch

--- lec/week6/UseEffects/src/components/FakeData.jsx
+++ lec/week6/UseEffects/src/components/FakeData.jsx
@@ -4,19 +4,25 @@ import { Wrap } from "./Wrapper";
 export function Fake() {
   const [quotes, setQuotes] = useState([]);
   useEffect(() => {
-    fetch("https://dummyjson.com/quotes").then(async (res) => {
+    // fetch("https://dummyjson.com/quotes").then(async (res) => {
+    fetch("https://poetrydb.org/lines/Love").then(async (res) => {
       const json = await res.json();
-      setQuotes(json.quotes);
+      setQuotes(json);
     });
   }, []);
 
   return (
     <div>
       {quotes.map((qt) => (
-        <Wrap key={qt.id}>
+        <Wrap>
           {/* <div>{qt.id}</div> */}
-          <div>{qt.id}.)  {qt.quote} - {qt.author}</div> <br></br>
+          {/* <div>{qt.id}.)  {qt.quote} - {qt.author}</div> <br></br> */}
           {/* <div>{qt.author}</div> */}
+
+          <div>title: {qt.title}</div>
+          <div>author: {qt.author}</div>
+          <div>poem: {qt.lines.map((line) => line)}</div>
+          <br></br>
         </Wrap>
       ))}
     </div>
