6ef2ce70deffbaa35d09dd59c1e789eb5d482529
Aditya Mishra <mishraaditya3456@gmail.com>
2025-09-22 03:52:09 +0530

    finally getting react

---
+++ lec/week6/Fullstack-app/Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Wrap } from "./components/Wrapper"
+import { Fetch } from "./components/FetchData"
+
+function App() {
+
+
+  return (
+    <>
+      <Wrap>
+        <Fetch />
+      </Wrap>
+    </>
+  )
+}
+
+export default App
---
+++ lec/week6/Fullstack-app/Frontend/src/components/FetchData.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { Wrap } from "./Wrapper";
+
+export function Fetch(){
+    const [todos, setTodos] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:4000/todos')
+        .then(async (res) => {
+            const json = await res.json();
+            setTodos(json.todos)
+        })
+    },[])
+
+    return(
+        <div>
+            {todos.map((todo, index) => (
+                <Wrap key={index}>
+                    {/* <div>{todo.title}</div>
+                    <div>{todo.description}</div>
+                    <div>{todo.status}</div> */}
+                    <div>{index+1}) {todo.title}</div>
+                    <div>description: {todo.description}</div>
+                    <div>status: {todo.status}</div> <br/>
+                </Wrap>
+            ))}
+        </div>
+    )
+}
