4c7f7800651593289065bb0e6aacc486a9bd5e91
iadii <mishraaditya3456@gmail.com>
2025-10-15 21:49:05 +0530

    updating dependency array

--- lec/week6/useCallback/src/components/Butts.jsx
+++ lec/week6/useCallback/src/components/Butts.jsx
@@ -3,7 +3,7 @@ import FetchData from "./FetchData"
 
 function Butt() {
 
-  const [click, setClick] = useState(0);
+  const [click, setClick] = useState(1);
 
 
   function adii(event) {
@@ -17,6 +17,8 @@ function Butt() {
       <button onClick={adii}>2</button>
       <button onClick={adii}>3</button>
       <button onClick={adii}>4</button>
+      <button onClick={adii}>5</button>
+      <br/> <br/>
       click is {click}
       <FetchData index={click -1} />
     </>
