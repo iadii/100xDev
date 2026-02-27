44662c4d464843cb7915d8f2de23051e9d6af916
iadii <mishraaditya3456@gmail.com>
2026-02-27 19:07:17 +0530

    added recoil

--- lec/week7/stateManagement/src/App.jsx
+++ lec/week7/stateManagement/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
 import { Count } from './components/Count'
-import { countContext } from './components/context'
+import { RecoilRoot } from 'recoil'
+ 
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
     <>
-      {/* wrap any componenet that want to use teleported value of user context inside the provider */}
-      <countContext.Provider value={{count, setCount}}>
-        <Count />
-      </countContext.Provider>
+        <RecoilRoot>
+          <Count />
+        </RecoilRoot>
     </>
   )
 }
--- lec/week7/stateManagement/src/components/Count.jsx
+++ lec/week7/stateManagement/src/components/Count.jsx
@@ -1,15 +1,7 @@
 import { Button } from "./Buttons"
 import { CountRenderer } from "./CountRenderer"
-import { countAtom } from "../store/atoms/count"
 
 export function Count() {
-    const count = 0;
-
-    console.log("even though this is not using this count component stil rerender")
-    console.log("this is problem of context api")
-    console.log("fix using state managemnet tool/library")
-    console.log("Unnessary re render")
-
     return (
         <div>
             <CountRenderer />
