82a68bec873e68411a441b55dc1e80b098f62569
iadii <mishraaditya3456@gmail.com>
2026-03-01 03:34:40 +0530

    wrote backend

--- lec/week7/recoil-fetch/backend/src/app.js
+++ lec/week7/recoil-fetch/backend/src/app.js
@@ -11,7 +11,7 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 app.use(cors({
-    origin: process.env.CORS_ORIGIN?.split(","),
+    origin: "*",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Authorization", "Content-Type"]
 }))
