9e8dbab03b82b365adc46ce2845518f50ad7928b
iadii <mishraaditya3456@gmail.com>
2026-09-12 22:16:42 +0530

    prac singup and login

--- assignment/paytm/backend/src/controller/user.controller.js
+++ assignment/paytm/backend/src/controller/user.controller.js
@@ -1,7 +1,32 @@
 import { User } from "../models/users.models.js";
+import { z } from "zod";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
+
+const signupSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+  firstName: z.string(),
+  lastName: z.string(),
+});
+const loginSchema = z.object({
+  username: z.string(),
+  password: z.string(),
+});
+
 const signup = async (req, res) => {
   const { username, password, firstName, lastName } = req.body;
+  const { success } = signupSchema.safeParse({
+    username,
+    password,
+    firstName,
+    lastName,
+  });
+  if (!success) {
+    return res.status(400).json({
+      msg: "Incorrect input",
+    });
+  }
 
   const existingUser = await User.findOne({ username });
   if (existingUser) {
@@ -24,31 +49,40 @@ const signup = async (req, res) => {
 
 const login = async (req, res) => {
   const { username, password } = req.body;
-  const user = User.findOne({username: username})
-  if(!user){
-    return res.status(404).json({
-        msg: `User not found`
-    })
+   const { success } = signupSchema.safeParse({
+    username,
+    password,
+  });
+  if (!success) {
+    return res.status(400).json({
+      msg: "Incorrect input",
+    });
   }
-  const isPasswordCorrect = await bcrypt.compare(password, user.password)
-  if(!isPasswordCorrect){
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(404).json({
+      msg: `User not found`,
+    });
+  }
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+  if (!isPasswordCorrect) {
     return res.status(401).json({
-        msg: `Invalid  Credentials`
-    })
+      msg: `Invalid  Credentials`,
+    });
   }
   // create jwt
   const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRY,
   });
-  res.status(201).json({
-    msg: `User created successfully`,
+  res.status(200).json({
+    msg: `Login successful`,
     data: {
-        id: user._id,
-        username,
-        Name: `${user.firstName} ${user.lastName}`,
-        token
-    }
-  })
+      id: user._id,
+      username,
+      Name: `${user.firstName} ${user.lastName}`,
+      token,
+    },
+  });
 };
 
 export { signup, login };
