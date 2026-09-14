ccc339896f94991a125e41621b1d95e0e3d3c4de
iadii <mishraaditya3456@gmail.com>
2026-09-14 22:15:26 +0530

    verify jwt tokens

--- assignment/paytm/backend/src/middleware/user.middleware.js
+++ assignment/paytm/backend/src/middleware/user.middleware.js
@@ -3,17 +3,22 @@ import jwt from "jsonwebtoken";
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(429).json({
-      msg: `Unauthorized`,
+    return res.status(401).json({
+       msg: "Authorization header is required",
     });
   }
   const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({
+      msg: "Access token is required",
+    });
+  }
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.userId;
   } catch (err) {
     return res.status(403).json({
-      msg: `something worng with token`,
+      msg: "Invalid or expired token",
     });
   }
 };
