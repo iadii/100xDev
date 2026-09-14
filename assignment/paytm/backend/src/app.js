0d2c323cf4fe84bd60e74ae1730e5ce829381f72
iadii <mishraaditya3456@gmail.com>
2026-09-14 22:24:52 +0530

    verify jwt tokens

--- assignment/paytm/backend/src/app.js
+++ assignment/paytm/backend/src/app.js
@@ -5,12 +5,11 @@ import healthRouter from './routes/health.routes.js'
 const app = express();
 
 
-app.use(express.json({ limit: "16kb" }))
-app.use(cors())
+app.use(cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 
-const prefix = '/api/v1'
+const prefix = '/api/v1';
 
 // auth
 app.use(`${prefix}/auth`, userRouter)
