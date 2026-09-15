a6bd74e9341680e99dcae92e6b51cdb10be6793f
iadii <mishraaditya3456@gmail.com>
2026-09-15 08:36:38 +0530

    feat: update-info route

--- assignment/paytm/backend/src/routes/users.routes.js
+++ assignment/paytm/backend/src/routes/users.routes.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import { login, signup } from '../controller/user.controller.js';
+import { login, signup, updateInfo } from '../controller/user.controller.js';
 const router = express.Router()
 
 
 router.post('/register', signup)
 router.post('/login', login)
-router.put('/update-info', login)
+router.put('/update-info', updateInfo)
 export default router;
