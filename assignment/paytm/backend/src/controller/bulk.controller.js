58581ce666e329be9c000ae7c20011f5df4889ab
iadii <mishraaditya3456@gmail.com>
2026-09-15 17:20:35 +0530

    feat: added bulk search query

---
+++ assignment/paytm/backend/src/controller/bulk.controller.js
@@ -0,0 +1,42 @@
+// it is for when user type /ad then shows result of name those starts with ad
+import { User } from "../models/users.models.js";
+
+const bulkQuery = async (req, res) => {
+  const filter = req.query.filter || "";
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          $regex: `^${filter}`,
+          $options: "i",
+        },
+      },
+      {
+        lastName: {
+          // this will find those words from everywhere like user search ad
+          //  then it will find every name contains ad in it Aditya, Brad, cadet, Adam
+          // "$regex": filter
+
+          $regex: `^${filter}`,
+
+          // $options: "i" means case-insensitive matching.
+          // if user type ad then without it only shows user's who put in small case
+          // but with it will find aditya, Adam, ADITYA, aDiTyA
+          $options: "i",
+        },
+      },
+    ],
+  });
+  res.status(200).json({
+    data: {
+      user: users.map((user) => ({
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        _id: user._id,
+      })),
+    },
+  });
+};
+
+export { bulkQuery };
--- assignment/paytm/backend/src/routes/users.routes.js
+++ assignment/paytm/backend/src/routes/users.routes.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import { login, signup } from '../controller/auth.controller.js';
 import { updateInfo } from '../controller/updateProfile.controller.js';
+import { bulkQuery } from '../controller/bulk.controller.js';
+
 
 const router = express.Router()
 
@@ -8,4 +10,5 @@ const router = express.Router()
 router.post('/register', signup)
 router.post('/login', login)
 router.put('/update-info', updateInfo)
+router.get('/bulk', bulkQuery)
 export default router;
